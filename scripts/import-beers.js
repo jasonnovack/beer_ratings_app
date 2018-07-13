739a111a6bded6e95b3e1603f718d20b96b3cbe2
Jason Novack <jason.novack@gmail.com>
2018-07-13 11:24:30 -0400

    modified import script to read from csv

--- scripts/import-beers.js
+++ scripts/import-beers.js
@@ -1,23 +1,19 @@
 const request = require("request");
-
-let beers = [
-	{
-		'name': 'Yuengling Traditional Lager',
-    'rating': 10
-	},
-  {
-    'name': 'Gulden Draak',
-    'rating': 10
-  }
-];
+const fs = require('fs');
+const parse = require('csv-parse');
+const delay = require('delay');
 
 const looper = async () => {
+  let ratings = await loadRatings();
+
   let matches = [];
   let noMatches = [];
-  for (let beer of beers) {
+  for (let rating of ratings) {
     try {
-      let response = await getBeer(beer);
-      matches.push(response);
+      await delay(500);
+      let beerWithId = await getBeer(rating);
+      let beerWithDetails = await getDetails(beerWithId);
+      matches.push(beerWithDetails);
     } catch(e) {
       noMatches.push(e);
     }
@@ -26,8 +22,28 @@ const looper = async () => {
   console.log('no matches:', JSON.stringify(noMatches, null, 2));
 };
 
+const loadRatings = async () => {
+  return new Promise((resolve, reject) => {
+    let beers = [];
+    const inputFile = './scripts/ratings-test.csv';
+
+    var parser = parse({delimiter: ','}, (err, data) => {
+      for (let line of data) {
+        beers.push({
+          'createdAt': Date.parse(line[0]),
+          'name': line[1],
+          'score': line[2],
+          'comment': line[3]
+        });
+      }
+      resolve(beers);
+    });
+    fs.createReadStream(inputFile).pipe(parser);
+  });
+};
 
 const getBeer = async (beer) => {
+  console.log('getBeer', beer.name);
   return new Promise(async (resolve, reject) => {
     var options = { 
       method: 'POST',
@@ -54,22 +70,8 @@ const getBeer = async (beer) => {
       } else {
         let items = body[0].data.results.items.filter(item => item.beer.name.toLowerCase() === beer.name.toLowerCase());
         if (items.length === 1) {
-          let details = await getDetails(beer, items[0].beer.id);
-          resolve({
-            'id': items[0].beer.id,
-            'name': items[0].beer.name,
-            'rating': beer.rating,
-            'brewer': details.data.beer.brewer.name,
-            'brewerType': details.data.beer.brewer.type,
-            'city': details.data.beer.brewer.city,
-            'state': details.data.beer.brewer.state.name,
-            'country': details.data.beer.brewer.country.name,
-            'style': details.data.beer.style.name,
-            'styleParent': details.data.beer.style.parent.name,
-            'abv': details.data.beer.abv,
-            'ibu': details.data.beer.ibu,
-            'rateBeerScore': details.data.beer.overallScore
-          });
+          beer.id = items[0].beer.id;
+          resolve(beer);
         } else {
           reject(beer);
         }
@@ -78,7 +80,7 @@ const getBeer = async (beer) => {
   });
 };
 
-const getDetails = async (beer, id) => {
+const getDetails = async (beer) => {
   return new Promise(async (resolve, reject) => {
     var options = {
       method: 'POST',
@@ -90,7 +92,7 @@ const getDetails = async (beer, id) => {
         Accept: 'application/json'
       },
       body: {
-        query: `query {beer(id: ${id}) {id overallScore name abv ibu brewer {name type country {name} city state {name}} style {name parent {name}}}}`,
+        query: `query {beer(id: ${beer.id}) {id overallScore name abv ibu brewer {name type country {name} city state {name}} style {name parent {name}}}}`,
         variables: '{}',
         operationName: null },
         json: true
@@ -101,7 +103,41 @@ const getDetails = async (beer, id) => {
           console.error(error);
           reject(beer);
         } else {
-          resolve(body);
+          if (body && body.data && body.data.beer) {
+            if (body.data.beer.brewer) {
+              if (body.data.beer.brewer.name && body.data.beer.brewer.name.length > 0) {
+                beer.brewer = body.data.beer.brewer.name;
+              }
+              if (body.data.beer.brewer.type && body.data.beer.brewer.type.length > 0) {
+                beer.brewerType = body.data.beer.brewer.type;
+              }
+              if (body.data.beer.brewer.city && body.data.beer.brewer.city.length > 0) {
+                beer.city = body.data.beer.brewer.city;
+              }
+              if (body.data.beer.abv) {
+                beer.abv = body.data.beer.abv;
+              }
+              if (body.data.beer.ibu) {
+                beer.ibu = body.data.beer.ibu;
+              }
+              if (body.data.beer.overallScore) {
+                beer.rateBeerScore = body.data.beer.overallScore;
+              }           
+            }
+            if (body.data.beer.brewer.state && body.data.beer.brewer.state.name && body.data.beer.brewer.state.name.length > 0) {
+              beer.state = body.data.beer.brewer.state.name;
+            }
+            if (body.data.beer.brewer.country.name && body.data.beer.brewer.country.name.length > 0) {
+              beer.country = body.data.beer.brewer.country.name;
+            }
+            if (body.data.beer.style.name && body.data.beer.style.name.length > 0) {
+              beer.style = body.data.beer.style.name;
+            }
+            if (body.data.beer.style.parent.name && body.data.beer.style.parent.name.length > 0) {
+              beer.styleParent = body.data.beer.style.parent.name;
+            }
+          }
+          resolve(beer);
         }
       });
   })
